ea482575177f27a2ced8f605e5dff4e7595a9fd0
Manol Dimitrov <manoldimtirov@yahoo.com>
2015-12-12 17:50:09 +0000

    First commit

---
+++ src/main/java/org/stravadataanalyser/StravaDataAnalyserApplication.java
@@ -0,0 +1,12 @@
+package org.stravadataanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StravaDataAnalyserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StravaDataAnalyserApplication.class, args);
+    }
+}
---
+++ src/test/java/org/stravadataanalyser/StravaDataAnalyserApplicationTests.java
@@ -0,0 +1,18 @@
+package org.stravadataanalyser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = StravaDataAnalyserApplication.class)
+@WebAppConfiguration
+public class StravaDataAnalyserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
